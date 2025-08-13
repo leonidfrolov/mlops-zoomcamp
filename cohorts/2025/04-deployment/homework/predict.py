a18f0e5df4eb38262941d2084b6a036177196e99
Leonid_Frolov1 <Leonid_Frolov1@epam.com>
2025-08-13 20:01:22 +0300

    Add module 4 homework

---
+++ cohorts/2025/04-deployment/homework/predict.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+import pandas as pd
+
+with open('model.bin', 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+def predict(features):
+    X = dv.transform(features)
+    preds = model.predict()
+    return preds
+
+categorical = ['PULocationID', 'DOLocationID']
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+    
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+df = read_data(input_file)
+
+df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+dicts = df[categorical].to_dict(orient='records')
+X_val = dv.transform(dicts)
+y_pred = model.predict(X_val)
---
+++ cohorts/2025/04-deployment/homework/starter.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+import pandas as pd
+import sys
+import os
+import sklearn
+
+sklearn.__version__
+
+year = int(sys.argv[1]) # 2023
+month = int(sys.argv[2]) # 3
+output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+
+os.makedirs('output', exist_ok=True)
+
+with open('model.bin', 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+categorical = ['PULocationID', 'DOLocationID']
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+    
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+df = read_data(input_file)
+
+df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+dicts = df[categorical].to_dict(orient='records')
+X_val = dv.transform(dicts)
+y_pred = model.predict(X_val)
+
+print(f'Q1: {y_pred.std()}')
+
+df_result = pd.DataFrame()
+df_result['ride_id'] = df['ride_id']
+df_result['predicted_duration'] = y_pred
+
+df_result.to_parquet(
+    output_file,
+    engine='pyarrow',
+    compression=None,
+    index=False
+)
+
+file_size_M = os.path.getsize(output_file) / 1024 / 1024
+print(f'Q2: {round(file_size_M, 2)}')
+
+print(f'Q3: jupyter nbconvert --to script starter.ipynb')
+
+print(f'Q1: {y_pred.mean()}')
+
